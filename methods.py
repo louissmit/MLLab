4b27ab24d83b4e9126a72740ba6c475c131b1adb
Louis Smit <mail@louissmit.com>
2013-10-02 02:10:38 +0200

    first plot

--- methods.py
+++ methods.py
@@ -1,6 +1,7 @@
 #%pylab inline
 import gzip, cPickle
 import matplotlib.pyplot as plt
+import numpy as np 
 
 
 def load_mnist():
@@ -17,5 +18,38 @@ def plot_digits(data, numcols, shape=(28,28)):
         plt.axis('off')
         plt.imshow(data[i].reshape(shape), interpolation='nearest', cmap='Greys')
     plt.show()
-    
 
+
+def logreg_gradient(x, t, W, b):
+    M,J=W.shape
+    N=x.shape
+    grad_b = np.zeros(J)
+    grad_W = np.zeros((M,J))
+    # compute Z
+    Z=0
+    for j in xrange(J):
+        Z+=np.exp(np.dot(W[:,j].T, x) + b[j])
+        # compute grad_b
+        grad_b[j]=-np.exp(np.dot(W[:,j].T, x) + b[j])
+    grad_b = grad_b / Z
+    grad_b[t]+=1
+
+    for j in xrange(J):
+        # compute grad_W
+        grad_W[:,j]=x*grad_b[j]
+        
+    return grad_b, grad_W
+
+def sgd_iter(x_train, t_train, W, b):
+    M,J=W.shape
+    N,M=x_train.shape
+    eta = 1E-4 #learning_rate
+    indices = np.arange(N,  dtype = int)
+    np.random.shuffle(indices)
+
+    # stochatic gradient decent
+    for n in indices:
+        grad_b, grad_W = logreg_gradient(x_train[n,:],t_train[n],W,b)
+        b+= eta*grad_b 
+        W+= eta*grad_W
+    return W,b
--- script.py
+++ script.py
@@ -1,6 +1,34 @@
 from methods import *
+import matplotlib.pyplot as plt
 
-
+# load data
 (x_train, t_train), (x_valid, t_valid), (x_test, t_test) = load_mnist()
+# plot_digits(x_train[0:8], numcols=4)
 
-plot_digits(x_train[0:8], numcols=4)
+#initilaize W and b
+N, M = x_train.shape
+J = 10
+b = np.zeros(J)
+W = np.zeros((M,J))
+
+# some iterations
+for i in xrange(5):
+    W, b = sgd_iter(x_train, t_train, W, b)
+    Z = 0
+    logq = np.zeros(J)
+    for j in xrange(J):
+        logq[j] = np.dot(W[:,j].T, x_train[i]) + b[j]
+        Z += np.exp(logq[j])
+    res = np.exp(logq - np.log(Z))
+    plt.plot(res, label="i = " + str(i))
+plt.legend()
+plt.show()
+
+
+    # prediction with x_test and t_test
+    # calculate error_test
+    # prediction with x_valid and t_valid
+    # calculate error_valid
+    # plot error of training/ validation
+    
+    # Visulaze W, W[:,j] is one image
