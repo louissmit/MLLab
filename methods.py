88e1d60b9cb0628e312f445c13d84df17fc4287a
Louis Smit <mail@louissmit.com>
2013-10-26 20:47:00 +0200

    wtf subplots

--- methods.py
+++ methods.py
@@ -58,15 +58,19 @@ def show_sample_kernels(X_test):
     N_test=len(X_test)
     zero_mean=np.zeros((N_test,))
 
-
-    for i in xrange(1):#xrange(len(THETAS)):
-        pp.title('theta='+str(THETAS[i]))
+    # pp.figure()
+    pp.subplots(2,3)
+    for i in xrange(len(THETAS)):
+        pp.title(r'$\theta$ ='+str(THETAS[i]))
         K=computeK(X_test, THETAS[i])
         y_test = np.random.multivariate_normal(zero_mean,K)
+        print i
+        pp.subplot(2,2,i+1)
         pp.plot(X_test,np.zeros(X_test.shape),'b--',label='expected mean')
         pp.plot(X_test,y_test,'r', label='GPP')
         pp.fill_between(X_test,y_test-2*np.diag(K)[1],y_test+2*np.diag(K)[1], alpha=0.15,facecolor='red')
         pp.legend()
+
 
 #_________________________________________________________________________
 # Predictive distribution
--- script.py
+++ script.py
@@ -5,76 +5,16 @@ Created on Wed Oct 09 12:08:57 2013
 @author: AlbertoEAF
 """
 from methods import *
+import matplotlib.pyplot as pp
 
-theta = np.array([[1,4,0,0], [9,4,0,0], [1,64,0,0], [1,0.25,0,0], [1,4,10,0], [1,4,0,5]])
-
-
-N_train = 2
-N_test = 100
-sigma=0.1
-X_train, T_train, X_test, y_train = data_gen(N_train,N_test,sigma)
-
-#_________________________________________________________________________
-# Predictive distribution
-
-
-mu, var = gp_predictive_distribution(X_train, T_train, X_test, theta[0], sigma)
-#gp_plot( X, y_train, mu, var, x_train, t_train, theta, beta )###needs to be edited tomorrow
-# ten data points
-
+X_train, T_train, X_true, Y_true, X_test = data_gen(15,30,sigma=None)
 
 #_________________________________________________________________________
 # Sampling from the Gaussian process prior
-show_sample_kernels(N_test, X_test, mu, 0)
+# We show for all 6 ThetaCompositions one sample from a Gaussian process prior (GPP). Also shown is the uncertainty of the 
+# GPP. In contrast to the book we only show one GPP sample not 5.
+
+show_sample_kernels(X_test)
+pp.show()
 
 #_________________________________________________________________________
-# Learning the hyperparameters
-    
-
-  
-K = computeK(X_train, theta[0])
-C = computeC(K, sigma) 
-Cinv=np.linalg.inv(C)
-
-#....
-
-#_________________________________________________________________________
-# 3.2 - Performs the grid-search
-
-def print_log_likelihood_result (result): 
-    print "Log-Likelihood:",result[0], "   Thetas:", result[1]
-
- def grid_search(X_train, T_train):   
-	#Choose the right values!! This search space might not be that relevant
-	thetas = [np.linspace(0,2,6), # theta0
-	          np.linspace(0,2,6), # theta1
-	          np.linspace(0,2,6), # theta2
-	          np.linspace(0,2,6)] # theta3
-
-	theta_combinations = gen_theta_combinations(thetas)
-	sigma=0.1
-	likelihood_results = []
-	for theta_combination in theta_combinations:
-	    likelihood_result, C, Cinv = gp_log_likelihood( X_train, T_train, theta_combination, sigma, C = None, invC = None )
-	    likelihood_results.append( (likelihood_result,theta_combination) )
-	    
-	likelihood_results = sorted(likelihood_results, key=lambda likelihood_result: likelihood_result[0])
-
-	# This is usually very big but it is asked.
-	for r in likelihood_results:
-	    print_log_likelihood_result(r)
-
-	print "Grid-search Best result:"
-	print_log_likelihood_result(likelihood_results[-1])
-	print "Grid-search Worst result:"
-	print_log_likelihood_result(likelihood_results[0])
-
-	print "plot-best-and-worst-results-KAREN :P:P(...)"
-
-#_________________________________________________________________________
-
-
-
-
-
-
